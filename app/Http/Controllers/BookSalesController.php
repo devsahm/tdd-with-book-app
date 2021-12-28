7ce19c05822ac715e835c4d5a2eaa92dbfd122b0
unknown <devsahm>
2021-12-28 19:28:03 +0100

    added intelli

--- app/Http/Controllers/BookSalesController.php
+++ app/Http/Controllers/BookSalesController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Jobs\IncreaseBookCopySoldJob;
 use App\Models\Book;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 
 class BookSalesController extends Controller
 {
